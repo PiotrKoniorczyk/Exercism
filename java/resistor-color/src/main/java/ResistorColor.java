deb7ef3016b9726f2b6d677bb1c38a403942ff93
PiotrKoniorczyk <piotrek.koniorczyk@gmail.com>
2020-04-06 01:14:30 +0200

    exercismo update

--- java/resistor-color/src/main/java/ResistorColor.java
+++ java/resistor-color/src/main/java/ResistorColor.java
@@ -1,9 +1,27 @@
 class ResistorColor {
     int colorCode(String color) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        int result = 0;
+        String[] colorList = new String[10];
+        colorList[0] = "Black";
+        colorList[1] = "Brown";
+        colorList[2] = "Red";
+        colorList[3] = "Orange";
+        colorList[4] = "Yellow";
+        colorList[5] = "Green";
+        colorList[6] = "Blue";
+        colorList[7] = "Violet";
+        colorList[8] = "Greyck";
+        colorList[9] = "White";
+        for (int i = 0; i < colorList.length; i++) {
+            if(colorList[i].equals(color)){
+                result = i;
+            }
+        }return result;
     }
 
     String[] colors() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        String[] colors = {"Black", "Brown", "Red", "Orange", "Yellow", "Green", "Blue", "Violet", "Grey"};
+
+        return colors;
     }
 }
