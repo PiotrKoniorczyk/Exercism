af96352fc5573efcf334a7418b557924735100de
PiotrKoniorczyk <piotrek.koniorczyk@gmail.com>
2020-04-07 01:40:10 +0200

    ExerciseA rmstrong number final version

--- java/armstrong-numbers/src/main/java/ArmstrongNumbers.java
+++ java/armstrong-numbers/src/main/java/ArmstrongNumbers.java
@@ -1,9 +1,15 @@
 class ArmstrongNumbers {
 
 	boolean isArmstrongNumber(int numberToCheck) {
+		int digitNumber = String.valueOf(numberToCheck).length();
+		String[] splitDigit =  String.valueOf(numberToCheck).split("");
 
-		throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-		
+		int result = 0;
+		for (int i = 0; i < digitNumber; i++) {
+			double add = Math.pow((Integer.valueOf(splitDigit[i])),digitNumber);
+			result += add;
+		}
+		return numberToCheck == result;
 	}
 
 }
