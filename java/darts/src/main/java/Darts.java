54abfc7e857d2c3e04da324b848227eb9486a958
PiotrKoniorczyk <piotrek.koniorczyk@gmail.com>
2020-04-07 00:42:09 +0200

    exercise - dart final

--- java/darts/src/main/java/Darts.java
+++ java/darts/src/main/java/Darts.java
@@ -1,11 +1,23 @@
 class Darts {
 
+    double x;
+    double y;
+
     Darts(double x, double y) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+        this.x = x;
+        this.y = y;}
 
     int score() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        int score = 0;
+        double point = Math.sqrt((x*x)+(y*y));
+        if (point <=1)
+            score += 10;
+        else if (point <=5 && point > 1)
+            score += 5;
+        else if (point <=10 && point > 5)
+            score += 1;
+
+        return score;
     }
 
 }
