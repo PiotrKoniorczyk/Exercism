32e80a565be233aae3fdf596b6f6c1af63ccaef1
PiotrKoniorczyk <piotrek.koniorczyk@gmail.com>
2020-04-08 21:51:40 +0200

    Exercise Raindrop Converter final

--- java/Test/src/Main.java
+++ java/Test/src/Main.java
@@ -2,6 +2,18 @@ import java.math.BigInteger;
 
 public class Main {
     public static void main(String[] args) {
-        BigInteger.
+        Acronym (String phrase) {
+            String acronym = "";
+            char[] pharseSplit = phrase.toCharArray();
+            for (int i = 0; i < phrase.length(); i++) {
+                if (i == 0) {
+                    acronym += String.valueOf(pharseSplit[i]);
+                } else if (pharseSplit[i] == ' ') {
+                    acronym += String.valueOf(pharseSplit[i + 1]);
+                }
+            }
+        }
+
+        System.out.println(acronim);
     }
 }
---
+++ java/acronym/src/main/java/Acronym.java
@@ -0,0 +1,21 @@
+class Acronym {
+
+    private String pharse;
+
+    Acronym(String phrase) {
+        this.pharse = phrase;
+    }
+
+    String get() {
+        StringBuilder acronym = new StringBuilder();
+        char[] pharseSplit = pharse.toCharArray();
+        for (int i = 0; i < pharse.length(); i++) {
+            if (i == 0) {
+                acronym.append(pharseSplit[i]);
+            } else if (pharseSplit[i] != ' ' && pharseSplit[i] != '-'){
+                acronym.append(pharseSplit[i]);
+            }
+        }
+        return acronym.toString().toUpperCase();
+    }
+}
