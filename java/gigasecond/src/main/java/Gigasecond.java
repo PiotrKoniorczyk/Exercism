38a1f8f19a09f3882d8e55231e158366ac489d51
PiotrKoniorczyk <piotrek.koniorczyk@gmail.com>
2020-04-08 14:59:03 +0200

    Exercise Gigasecond final

---
+++ java/gigasecond/src/main/java/Gigasecond.java
@@ -0,0 +1,20 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+class Gigasecond {
+    private LocalDateTime moment;
+
+
+    Gigasecond(LocalDate moment) {
+        this(moment.atStartOfDay());
+    }
+
+    Gigasecond(LocalDateTime moment) {
+        this.moment = moment;
+    }
+
+    LocalDateTime getDateTime() {
+        return moment.plusSeconds(1000000000);
+    }
+
+}
