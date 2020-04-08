87877154a6779e00f516eaad26c96e3939f190ff
PiotrKoniorczyk <piotrek.koniorczyk@gmail.com>
2020-04-08 21:49:22 +0200

    Exercise Raindrop Converter

---
+++ java/raindrops/src/main/java/RaindropConverter.java
@@ -0,0 +1,15 @@
+class RaindropConverter {
+
+    String convert(int number) {
+        StringBuilder convert = new StringBuilder();
+        if(number%3 == 0) {
+            convert.append("Pling");
+        }if(number%5 == 0){
+            convert.append("Plang");
+        }if(number%7 == 0){
+            convert.append("Plong");
+        }else if(number%3 != 0 && number%5 != 0 && number%7 != 0){convert.append(number);
+    }
+        return String.valueOf(convert);
+    }
+}
