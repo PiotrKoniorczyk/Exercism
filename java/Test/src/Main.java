02644ef0e44977b483d78244ab15ea5e4c1e2616
PiotrKoniorczyk <piotrek.koniorczyk@gmail.com>
2020-04-11 17:37:07 +0200

    Exercise final version

--- java/Test/src/Main.java
+++ java/Test/src/Main.java
@@ -1,12 +1,26 @@
+class Proverb {
 
+    private StringBuilder result = new StringBuilder();
+    private String[] wordsList = {"shoe", "horse", "rider", "message", "battle", "kingdom", "nail"};
 
-public class Main{
-    public static void main(String[] args) {
+    Proverb(String[] words) {
+        String[] rhyme = new String[7];
+        rhyme[0] = "For want of a nail the shoe was lost.\n";
+        rhyme[1] = "For want of a shoe the horse was lost.\n";
+        rhyme[2] = "For want of a horse the rider was lost.\n";
+        rhyme[3] = "For want of a rider the message was lost.\n";
+        rhyme[4] = "For want of a message the battle was lost.\n";
+        rhyme[5] = "For want of a battle the kingdom was lost.\n";
+        rhyme[6] = "And all for the want of a nail.";
 
-        Test_2 test = new Test_2(new int[]{ 1, 1, 1, 3, 5 }, YachtCategory.THREES);
-        System.out.println(test.score());
-
-
-
+        for (int i = 0; i < wordsList.length; i++) {
+            for (int j = 0; j < words.length ; j++) {
+                if (wordsList[i].equals(words[j])){
+                    result.append(rhyme[i]);
+                }
+            }
+        }
     }
+    String recite() {
+        return String.valueOf(result); }
 }
--- java/proverb/src/main/java/Proverb.java
+++ java/proverb/src/main/java/Proverb.java
@@ -1,33 +1,15 @@
-
 class Proverb {
-    public enum keywoard  {SHOE, HORSE, RIDER, MESSAGE, BATTLE, KINGDOM, NAIL};
 
-
-    private StringBuilder result;
+    private StringBuilder result = new StringBuilder();
 
     Proverb(String[] words) {
-        String[] rhyme = new String[7];
-
-        rhyme[0] = "For want of a nail the shoe was lost.\n";
-        rhyme[1] = "For want of a shoe the horse was lost.\n";
-        rhyme[2] = "For want of a horse the rider was lost.\n";
-        rhyme[3] = "For want of a rider the message was lost.\n";
-        rhyme[4] = "For want of a message the battle was lost.\n";
-        rhyme[5] = "For want of a battle the kingdom was lost.\n";
-        rhyme[6] = "And all for the want of a nail.";
-
-
-        for(String x:rhyme){
-            if(words.equals("shoe")){
-                result.append()
+        if(words.length>1) {
+            for (int i = 0; i < words.length-1; i++) {
+                result.append("For want of a ").append(words[i]).append(" the ").append(words[i + 1]).append(" was lost.\n");
             }
-
         }
+        if(words.length != 0) result.append("And all for the want of a ").append(words[0]).append(".");
     }
-
-
-
     String recite() {
-        return result; }
-
+        return String.valueOf(result); }
 }
