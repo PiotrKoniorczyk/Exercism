ea27fd4bc5170f951f0348162bc1946d27601c30
PiotrKoniorczyk <piotrek.koniorczyk@gmail.com>
2020-04-14 22:00:35 +0200

    Exercise final version

--- java/matrix/src/main/java/Matrix.java
+++ java/matrix/src/main/java/Matrix.java
@@ -1,15 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
 
 class Matrix {
+    String[] matrixAsString_split;
+    int countColumn;
+
 
     Matrix(String matrixAsString) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        matrixAsString_split = matrixAsString.split("\n");
+
+        countColumn = matrixAsString_split.length;
     }
 
     int[] getRow(int rowNumber) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+
+        List<Integer> rowList = new ArrayList<>();
+        String[] inRow = matrixAsString_split[rowNumber - 1].split(" ");
+        for (String s : inRow) {
+            rowList.add(Integer.parseInt(s));
+        }
+        return rowList.stream().mapToInt(i -> i).toArray();
     }
 
     int[] getColumn(int columnNumber) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        List<Integer> columnList = new ArrayList<>();
+
+        for (int i = 0; i < countColumn; i++){
+            String[] inRow = matrixAsString_split[i].split(" ");
+            String inColumn = inRow[columnNumber-1];
+            columnList.add(Integer.parseInt(inColumn));
+        }
+        int[] columnFinal = columnList.stream().mapToInt(i -> i).toArray();
+
+        return columnFinal;
     }
 }
